cb2f1318723fdf8110a29156688af0fdd3f47060
megantimmes <mktimmes@wm.edu>
2025-10-16 13:40:15 -0400

    changed some text

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ st.markdown(f"""
         <span class='gradient-text'>{page_heading}</span>
         <h5 style='text-align:center; color:#37474F;'>
             {(
-                "Discover the ocean's mysteries. Upload a photo to instantly identify a benthic species with AI-powered recognition."
+                "Discover the ocean's mysteries. Classify benthic species in underwater images with AI-powered recognition."
                 if st.session_state.page == "Home"
                 else "Use this model to analyze marine images and detect or classify benthic species."
             )}
@@ -435,12 +435,12 @@ if st.session_state.page == "Home":
                 color: #04365c;
             ">
                 <h1 style="font-size: 2.6rem; font-weight: 800; color: #0D47A1; margin-bottom: 1rem;">
-                    Welcome to Benthic AI 🌊
+                    Welcome to AMplify AI 🌊
                 </h1>
                 <p style="font-size: 1.2rem; line-height: 1.7;">
-                    Explore the ocean’s mysteries using AI-powered marine classification and detection.<br><br>
-                    Choose <b>Classification</b> to identify species in your photos, or <b>Detection</b> 
-                    to analyze objects in underwater scenes.
+                    We offer <b>2</b> finetuned deep-learning models for marine life classification and detection.<br><br>
+                    Choose our <b>Classification</b> model to identify benthic species in your photos, or our <b>Detection</b> model
+                    to box and classify the benthic species in underwater scenes.
                 </p>
             </div>
         </div>
